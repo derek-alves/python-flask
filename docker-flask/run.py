3cc2edd140d0fbc4d86c3c104c18d7e35fffc3e0
derek-alves <derek.alves1998@hotmail.com>
2021-07-24 07:40:26 -0300

    connection of db

--- docker-flask/run.py
+++ docker-flask/run.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from src import UserRepo
+from flask import Flask, request
 app = Flask(__name__)
+
 
 @app.route("/", methods=["GET"])
 def hello_world():
     return "OLa, estou an aplicação"
+
+
+@app.route("/", methods=["POST"])
+def insert():
+    userRepo = UserRepo()
+    body = request.json
+
+    userRepo.insert_user(body['name'])
+    return "OK"
